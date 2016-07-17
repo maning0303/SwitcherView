efd67daa6269321e7254a382854c51387567b3b6
马宁 <154292322@qq.com>
2016-07-17 12:45:24 +0800

    first commit

---
+++ app/src/main/java/com/maning/switcherview/MainActivity.java
@@ -0,0 +1,49 @@
+package com.maning.switcherview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.maning.library.SwitcherView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "------";
+    private SwitcherView switcherView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        switcherView = (SwitcherView) findViewById(R.id.switcherView);
+
+        ArrayList<String> strs = new ArrayList<>();
+        strs.add("哎呦，不错哦");
+        strs.add("你知道我是谁吗你知道我是谁吗你知道我是谁吗");
+        strs.add("哈哈哈");
+        strs.add("1111111111111");
+        switcherView.setResource(strs);
+        switcherView.startRolling();
+
+        switcherView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "-------onclick");
+                Toast.makeText(MainActivity.this, switcherView.getCurrentItem(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        switcherView.destroySwitcher();
+        super.onDestroy();
+    }
+}
---
+++ library/src/main/java/com/maning/library/SwitcherView.java
@@ -0,0 +1,151 @@
+package com.maning.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by maning on 16/7/17.
+ * 垂直滚动的广告栏
+ */
+public class SwitcherView extends TextSwitcher implements ViewSwitcher.ViewFactory, View.OnTouchListener {
+
+    private static final String TAG = "--------------";
+
+    private Handler handler = new Handler();
+    private Timer timer;   //计时器
+
+    private ArrayList<String> dataSource = new ArrayList<>();  //数据源
+    private int currentIndex = 0;   //滚动的位置
+    private float textSize = 16;    //默认文字大小
+    private int textColor = 0xFF000000; //默认颜色
+    private int timePeriod = 3000;  //时间周期
+    private String defaultText = "";  //默认文字显示
+    private boolean flag = true;
+
+    private TextView tView;
+
+
+    public SwitcherView(Context context) {
+        this(context, null);
+    }
+
+    public SwitcherView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.SwitcherView);
+        textColor = ta.getColor(R.styleable.SwitcherView_switcherTextColor, textColor);
+        timePeriod = ta.getInt(R.styleable.SwitcherView_switcherRollingTime, timePeriod);
+        textSize = ta.getDimension(R.styleable.SwitcherView_switcherTextSize, textSize);
+        defaultText = TextUtils.isEmpty(ta.getString(R.styleable.SwitcherView_switcherDefaultText)) ? "" : ta.getString(R.styleable.SwitcherView_switcherDefaultText);
+        ta.recycle();
+
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public View makeView() {
+        tView = new TextView(getContext());
+        tView.setTextSize(textSize);
+        tView.setTextColor(textColor);
+        tView.setText(defaultText);
+        tView.setSingleLine();
+        tView.setPadding(10, 5, 10, 5);
+        tView.setEllipsize(TextUtils.TruncateAt.END);
+        return tView;
+    }
+
+    public void setResource(ArrayList<String> dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (flag) {
+                        updateTextSwitcher();
+                    }
+                }
+            });
+        }
+    };
+
+    private void updateTextSwitcher() {
+        if (dataSource != null && dataSource.size() > 0) {
+            this.setText(dataSource.get(currentIndex++));
+            if (currentIndex > dataSource.size() - 1) {
+                currentIndex = 0;
+            }
+        }
+    }
+
+    public void startRolling() {
+        if (timer == null) {
+            this.setFactory(this);
+            this.setInAnimation(getContext(), R.anim.m_switcher_vertical_in);
+            this.setOutAnimation(getContext(), R.anim.m_switcher_vertical_out);
+            timer = new Timer();
+            timer.schedule(timerTask, timePeriod, timePeriod);
+        }
+    }
+
+    public String getCurrentItem() {
+        if (dataSource != null && dataSource.size() > 0) {
+            return dataSource.get(getCurrentIndex());
+        } else {
+            return "";
+        }
+    }
+
+    public int getCurrentIndex() {
+        int index = currentIndex - 1;
+        if (index < 0) {
+            index = dataSource.size() - 1;
+        }
+        return index;
+    }
+
+    public void destroySwitcher() {
+        handler.removeCallbacksAndMessages(null);
+        handler = null;
+        if (timer != null) {
+            timer.cancel();
+            timerTask.cancel();
+            timer = null;
+            timerTask = null;
+        }
+        if (dataSource != null && dataSource.size() > 0) {
+            dataSource.clear();
+            dataSource = null;
+        }
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            flag = false;
+        }
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            flag = true;
+        }
+        return false;
+    }
+}
